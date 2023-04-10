05b852366b983b5650370c05bf6d55048bd17ea7
Alan Zhan <ayz975@gmail.com>
2023-04-09 22:45:25 -0400

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb+srv://ayz975:<ilovecars>@cluster0.bac1ven.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_STRING);
 
 function App() {
   return (
